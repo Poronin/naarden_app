a16c2ff15d893755482b7959901113a437d9e2e0
Leo <leonard0@duck.com>
2022-10-01 14:56:32 +0200

    ̈́db tables created for Columns Tables and Users

--- naarden/models.py
+++ naarden/models.py
@@ -10,33 +10,38 @@ class Users(db.Model, UserMixin):
     hash = db.Column(db.Text(), nullable=False)
     table_id = db.relationship('Tables', backref='user_tables', lazy=True)
     column_id = db.relationship('Columns', backref='user_columns', lazy=True)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}',{self.hash})"
 
 
 class Tables(db.Model):
     __tablename__ = 'tables'
-    #id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
-    table_name = db.Column(db.Text(), nullable=False, primary_key=True)
-    logical_entity = db.Column(db.Text(), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'),primary_key=True)
+    table_name = db.Column(db.Text(), primary_key=True)
+    logical_entity = db.Column(db.Text(), nullable=False)
     description = db.Column(db.Text(), nullable=True)
     category = db.Column(db.String(20), nullable=True)
+    column_id = db.relationship('Columns', backref='table_columns', lazy=True)
+    
+    def __repr__(self):
+        return f"User('{self.user_id}', '{self.table_name}','{self.logical_entity}', '{self.description}','{self.category}')"
 
 
 class Columns(db.Model):
     __tablename__ = 'columns'
-    #id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
-    column_name = db.Column(db.Text(), nullable=False, primary_key=True)
-    logical_entity = db.Column(db.Text(), nullable=True)
+    table_name = db.Column(db.Text, db.ForeignKey('tables.table_name'), primary_key=True)
+    column_name = db.Column(db.Text(), primary_key=True)
+    logical_entity = db.Column(db.Text(), nullable=False)
     description = db.Column(db.Text(), nullable=True)
     type = db.Column(db.String(20), nullable=True)
     length = db.Column(db.String(10), nullable=True)
     key = db.Column(db.String(1), nullable=True)
     virtual = db.Column(db.String(1), nullable=True)
 
-
-def __repr__(self):
-    return f"User('{self.username}', '{self.email}',{self.hash})"
+    def __repr__(self):
+        return f"User('{self.user_id}', '{self.table_name}', '{self.column_name}','{self.logical_entity}', '{self.description}','{self.type}', '{self.length}', '{self.key}','{self.virtual}')"
 
 
 @login_manager.user_loader
